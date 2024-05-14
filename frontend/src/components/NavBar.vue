ecbc3ce087ab5b6a886d5c67cb8ad8e9e97ebb19
Rocio-Ch <rociomagali77@gmail.com>
2024-05-14 19:42:09 -0300

    create new NavBar component with html elements and styles

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-toolbar color="#101510">
+        <RouterLink to="/" class="navLink" style="display: flex; align-items: center;">
+            <span class="material-icons diamond">diamond</span>
+            <v-toolbar-title class="toolbar-title">
+                Mednat-SSR
+            </v-toolbar-title>
+        </RouterLink>
+        <v-layout 
+        active
+        class="overflow-visible navBar-menu"
+        >
+
+        <RouterLink to="/" class="navLink">
+            <v-btn
+            class="m-2"
+            height="50px"
+            dark
+            >
+                Home
+            </v-btn>
+        </RouterLink>
+        
+        <RouterLink to="/tasks" class="navLink">
+            <v-btn
+            class="m-2"
+            height="50px"
+            dark
+            >
+                Tasks
+            </v-btn>
+        </RouterLink>
+        </v-layout>
+    </v-toolbar>
+
+</template>
+
+<script setup>
+
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<style scoped>
+
+.navLink{
+    color: white;
+    text-decoration: none;
+}
+
+.diamond {
+    font-size: 45px;
+    color: #43d1f4;
+}
+
+.toolbar-title {
+    font-weight: 500;
+    font-size: 27px;
+    margin-left: 5px;
+}
+
+.navBar-menu {
+    justify-content: end;
+    margin-right: 10px;
+}
+
+</style>
