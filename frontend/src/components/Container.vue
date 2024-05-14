33a695b28d4e8d0da6792fae34015e85caf50a31
Rocio-Ch <rociomagali77@gmail.com>
2024-05-14 19:39:21 -0300

    add content and styles to container component, add image in assets folder

--- frontend/src/components/Container.vue
+++ frontend/src/components/Container.vue
@@ -1,9 +1,50 @@
 <template>
-
-
+    <div class="banner-container">
+        <h1 class="title">
+            ¡Bienvenido a MEDNAT-SSR!
+        </h1>
+        <h2 class="title2">
+            Plataforma N° 1 en gestion de tareas
+        </h2>
+    </div>
 </template>
 
 
 <script setup>
-
+//
 </script>
+
+<style scoped>
+
+.banner-container {
+    position: relative;
+    background-image: url("../assets/pattern-banner.jpg");
+    background-size: cover;
+    height: calc(100% - 114px);
+}
+
+.container {
+    height: calc(100% - 64px);
+    text-align: center;
+}
+
+.title {
+    position: absolute;
+    top: 30%;
+    margin-left: 100px;
+    font-family: "Poppins", sans-serif;
+    font-size: 3rem;
+    color: black;
+}
+
+.title2 {
+    position: absolute;
+    top: 38%;
+    margin-left: 120px;
+    margin-top: 20px;
+    font-family: "Poppins", sans-serif;
+    font-size: 1.5rem;
+    color: black;
+}
+
+</style>
