782c41e33c6c676fd772431b930a1d8148821725
Rocio-Ch <rociomagali77@gmail.com>
2024-05-14 19:46:27 -0300

    import components in App and RouterView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,13 @@
 <template>
-
+    <NavBar />
+    <RouterView />
+    <Footer />
 </template>
 
 <script setup>
   
+  import { RouterView } from 'vue-router'
+  import NavBar from './components/NavBar'
+  import Footer from './components/Footer'
 
 </script>
